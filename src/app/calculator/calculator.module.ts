10ba771777d0b2252fbd31435953b8abfae3df24
Wojciech GOLEWSKI <wojciech.golewski@soprasteria.com>
2020-01-10 07:48:52 +0100

    calculator-frame component generated

--- src/app/calculator/calculator.module.ts
+++ src/app/calculator/calculator.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CalculatorRoutingModule } from './calculator-routing.module';
+import { CalculatorFrameComponent } from './components/calculator-frame/calculator-frame.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CalculatorFrameComponent],
   imports: [
     CommonModule,
     CalculatorRoutingModule
