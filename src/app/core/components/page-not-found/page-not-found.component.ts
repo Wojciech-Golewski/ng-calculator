2875d3af434280d121b20810a61101a2a651218f
Wojciech GOLEWSKI <wojciech.golewski@soprasteria.com>
2020-01-10 07:56:21 +0100

    initial HTML markup and logic code to the page-not-found added

--- src/app/core/components/page-not-found/page-not-found.component.ts
+++ src/app/core/components/page-not-found/page-not-found.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss']
 })
-export class PageNotFoundComponent implements OnInit {
+export class PageNotFoundComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private redirection: ReturnType<typeof setTimeout>;
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.redirection = setTimeout(() => {
+      this.router.navigateByUrl('/');
+    }, 5000);
+  }
+
+  ngOnDestroy() {
+    if (this.redirection) {
+      clearTimeout(this.redirection);
+    }
   }
 
 }
