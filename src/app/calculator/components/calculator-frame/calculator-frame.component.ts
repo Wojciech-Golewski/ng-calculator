b8440f576e0fea718f8199ff0771215a03ec6676
Wojciech GOLEWSKI <wojciech.golewski@soprasteria.com>
2020-01-10 09:58:13 +0100

    initial structure for calculator implemented

--- src/app/calculator/components/calculator-frame/calculator-frame.component.ts
+++ src/app/calculator/components/calculator-frame/calculator-frame.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CalculatorService } from '../../services/calculator.service';
 
 @Component({
   selector: 'app-calculator-frame',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorFrameComponent implements OnInit {
 
-  constructor() { }
+  numbers: number[] = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+
+  operatorList: string[] = ['/', 'x', '-', '+'];
+
+  constructor(
+    private calculatorService: CalculatorService
+  ) { }
 
   ngOnInit() {
+  }
+
+  takeNumber(number: number) {
+    console.log(number);
+  }
+
+  computate() {
+    console.log('computating');
+  }
+
+  takeOperator(operator: string) {
+    const correctOperator = this.calculatorService.checkOperator(operator);
+    console.log(correctOperator);
+    console.log(typeof correctOperator);
   }
 
 }
